13b5f82a28a570af22fe289051fa76699a8560dd
Luis Felipe Sanchez Vega <1007753235@academico.icesi.edu.co>
2020-02-04 14:58:02 -0500

    operations ready

---
+++ src/model/SetOperations.java
@@ -0,0 +1,53 @@
+package model;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class SetOperations {
+
+	public SetOperations() {
+
+
+
+	}
+
+	public Set<String> union(Set<String> setA, Set<String> setB){
+
+		Set<String> setC = new HashSet<String>();
+
+		setC.addAll(setA);
+		setC.addAll(setB);
+
+		return setC;
+
+	}
+
+	public Set<String> intersection(Set<String> setA, Set<String> setB){
+
+		Set<String> setC = new HashSet<String>();
+
+		for (String e : setA) {
+			if (setB.contains(e)) {
+				setC.add(e);
+			}
+		}
+
+		return setC;
+	}
+
+	public Set<String> diference(Set<String> setA, Set<String> setB){
+
+		Set<String> setC = new HashSet<String>();
+
+		for (String e : setA) {
+			if (!setB.contains(e)) {
+				setC.add(e);
+			}
+		}
+
+		return setC;
+	}
+	
+	
+
+}
