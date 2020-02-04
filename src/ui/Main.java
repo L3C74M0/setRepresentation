d921a7b5e0ac4f07b497605afe6a7f8282019f8e
Juan David Lectamo Caicedo <1006219058@academico.icesi.edu.co>
2020-02-04 14:30:09 -0500

    Main

--- src/ui/Main.java
+++ src/ui/Main.java
@@ -1,10 +1,23 @@
 package ui;
 
-public class Main {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
+public class Main extends Application{	
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		launch(args);
 	}
 
+	@Override
+	public void start(Stage stage) throws Exception {
+		Parent root= FXMLLoader.load(getClass().getResource("setGUI.fxml"));
+       	Scene scene= new Scene(root);
+       	stage.setTitle("Conjuntos");
+       	stage.setScene(scene);
+       	stage.show();
+	}
 }
