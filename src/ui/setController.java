71234a45182c3b149fd683304c832e4b192542b2
Juan David Lectamo Caicedo <1006219058@academico.icesi.edu.co>
2020-02-04 15:58:11 -0500

    Documentado controller y agregadas algunas recomendaciones

--- src/ui/setController.java
+++ src/ui/setController.java
@@ -4,8 +4,6 @@ import java.net.URL;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.ResourceBundle;
-import java.util.Set;
-
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
@@ -19,7 +17,6 @@ import javafx.scene.layout.Region;
 import model.SetOperations;
 
 public class setController {
-	
 
 	@FXML
 	private ResourceBundle resources;
@@ -32,12 +29,12 @@ public class setController {
 
 	@FXML
 	private ComboBox<String> setB;
-	
-    @FXML
-    private Label uno;
 
-    @FXML
-    private Button dos;
+	@FXML
+	private Label uno;
+
+	@FXML
+	private Button dos;
 
 	@FXML
 	private ComboBox<String> operator;
@@ -55,121 +52,121 @@ public class setController {
 
 	@FXML
 	private TextField set;
-	
+
 	/**
 	 * The method adds sets A and B to be able to operate them
 	 */
 	@FXML
 	void addSet(ActionEvent event) {
-		if (numSet<1) {
-			String line=set.getText();
-			String[] args=line.split(",");
-			if (numSet==0) {
+		if (numSet < 1) {
+			String line = set.getText();
+			String[] args = line.split(",");
+			if (numSet == 0) {
 				for (int i = 0; i < args.length; i++) {
-					a= new ArrayList<String>();
-				 a.add(args[i]);
+					a.add(args[i]);
 				}
-			}else {
+			} else {
 				for (int i = 0; i < args.length; i++) {
-					b=new ArrayList<String>();
 					b.add(args[i]);
 				}
-				set.setText("");
 			}
+			set.setText("");
 			numSet++;
-    	}else {
-			
-    		Alert alert = new Alert(AlertType.INFORMATION, "Ya no puede crear más conjuntos", ButtonType.OK);
+		} else {
+			Alert alert = new Alert(AlertType.INFORMATION, "Ya no puede crear más conjuntos", ButtonType.OK);
 			alert.getDialogPane().setMinHeight(Region.USE_PREF_SIZE);
 			alert.show();
 			set.setVisible(false);
-    	}
+			uno.setVisible(false);
+			dos.setVisible(false);
+		}
 	}
 
 	/**
-	 * According to what the user chooses, he operates the sets created by calling the model.
+	 * According to what the user chooses, he operates the sets created by calling
+	 * the model.
 	 */
 	@FXML
-    void operate(ActionEvent event) {	
-    	boolean continuar = true;
-    	
-    	if(setA.getValue() == null) {
-    		Alert alert = new Alert(AlertType.ERROR, "Por favor seleccione un conjunto A", ButtonType.OK);
+	void operate(ActionEvent event) {
+		boolean continuar = true;
+
+		if (setA.getValue() == null) {
+			Alert alert = new Alert(AlertType.ERROR, "Por favor seleccione un conjunto A", ButtonType.OK);
 			alert.getDialogPane().setMinHeight(Region.USE_PREF_SIZE);
 			alert.show();
 			continuar = false;
-    	}
-    	
-    	if(setB.getValue() == null) {
-    		Alert alert = new Alert(AlertType.ERROR, "Por favor seleccione un conjunto B", ButtonType.OK);
+		}
+
+		if (setB.getValue() == null) {
+			Alert alert = new Alert(AlertType.ERROR, "Por favor seleccione un conjunto B", ButtonType.OK);
 			alert.getDialogPane().setMinHeight(Region.USE_PREF_SIZE);
 			alert.show();
 			continuar = false;
-    	}
-    	
-    	if(operator.getValue() == null) {
-    		Alert alert = new Alert(AlertType.ERROR, "Por favor seleccione un operador", ButtonType.OK);
+		}
+
+		if (operator.getValue() == null) {
+			Alert alert = new Alert(AlertType.ERROR, "Por favor seleccione un operador", ButtonType.OK);
 			alert.getDialogPane().setMinHeight(Region.USE_PREF_SIZE);
 			alert.show();
 			continuar = false;
-    	}
-    	
-    	
-    	if (continuar) {
-    		
-    		Set<String> set1 = new HashSet<String>(a);
-    		Set<String> set2 = new HashSet<String>(b);
-    		HashSet<String> set;
-			
-    		if (operator.getValue().equalsIgnoreCase("Union")) {
-    			
-    			set = so.union(set1, set2);
-				
-			}else if (operator.getValue().equalsIgnoreCase("Intersecion")) {
-				
-				 set = so.intersection(set1, set2);
-				
-			}else if (operator.getValue().equalsIgnoreCase("Diferencia")) {
-				
-				 set = so.diference(set1, set2);
-				
-			}else {
-				 set = so.diference(set2, set1);
+		}
+
+		if (continuar) {
+
+			HashSet<String> set1 = new HashSet<String>(a);
+			HashSet<String> set2 = new HashSet<String>(b);
+			HashSet<String> set;
+
+			if (operator.getValue().equalsIgnoreCase("Union")) {
+
+				set = so.union(set1, set2);
+
+			} else if (operator.getValue().equalsIgnoreCase("Intersecion")) {
+
+				set = so.intersection(set1, set2);
+
+			} else if (operator.getValue().equalsIgnoreCase("Diferencia")) {
+
+				set = so.diference(set1, set2);
+
+			} else {
+				set = so.diference(set2, set1);
 			}
-    		
-    		String message = "";
-    	
-    		
-    		for (String f : set) {
+
+			String message = "";
+
+			for (String f : set) {
 				message += f;
 			}
-    		
-    		result.setText(message);
-		}	
-    }
-	
-	
+
+			result.setText(message);
+		}
+	}
+
 	/**
 	 * This method is responsible for initializing the elements in the user
 	 * interface, sets and operators.
 	 */
-    @FXML
-    void initialize() {
-    	setA.getItems().add("A");
-    	setA.getItems().add("A'");
-    	setA.getItems().add("B");
-    	setA.getItems().add("B'");
-    	
-    	setB.getItems().add("A");
-    	setB.getItems().add("A'");
-    	setB.getItems().add("B");
-    	setB.getItems().add("B'");
-    	
-    	operator.getItems().add("Intersecion");
-    	operator.getItems().add("Union");
-    	operator.getItems().add("Complemento");
-    	operator.getItems().add("Diferencia");
-    	
-    	so = new SetOperations();
-    }
+	@FXML
+	void initialize() {
+		setA.getItems().add("A");
+		setA.getItems().add("A'");
+		setA.getItems().add("B");
+		setA.getItems().add("B'");
+
+		setB.getItems().add("A");
+		setB.getItems().add("A'");
+		setB.getItems().add("B");
+		setB.getItems().add("B'");
+
+		operator.getItems().add("Intersecion");
+		operator.getItems().add("Union");
+		operator.getItems().add("Complemento");
+		operator.getItems().add("Diferencia");
+
+		so = new SetOperations();
+
+		a = new ArrayList<String>();
+		b = new ArrayList<String>();
+	}
 }
