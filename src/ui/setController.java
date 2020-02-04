2cca025c450f5de8243f087a3a0609ebfac3df38
Juan David Lectamo Caicedo <1006219058@academico.icesi.edu.co>
2020-02-04 15:43:49 -0500

    Documentado controller y agregadas algunas recomendaciones

--- src/ui/setController.java
+++ src/ui/setController.java
@@ -7,6 +7,7 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.Button;
 import javafx.scene.control.ButtonType;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
@@ -16,87 +17,92 @@ import model.SetOperations;
 
 public class setController {
 
-    @FXML
-    private ResourceBundle resources;
+	@FXML
+	private ResourceBundle resources;
 
-    @FXML
-    private URL location;
+	@FXML
+	private URL location;
 
-    @FXML
-    private ComboBox<String> setA;
+	@FXML
+	private ComboBox<String> setA;
 
+	@FXML
+	private ComboBox<String> setB;
+	
     @FXML
-    private ComboBox<String> setB;
-
-    @FXML
-    private ComboBox<String> operator;
-
-    @FXML
-    private Label result;
-
-    private int numSet=0;
-    
-    private SetOperations so;
-    
-    private ArrayList<String> a;
-    
-    private ArrayList<String> b;
+    private Label uno;
 
     @FXML
-    private TextField set;
+    private Button dos;
 
+	@FXML
+	private ComboBox<String> operator;
 
-    @FXML
-    void addSet(ActionEvent event) {
-    	if (numSet<3) {
-			
+	@FXML
+	private Label result;
+
+	private int numSet = 0;
+
+	private SetOperations so;
+
+	private ArrayList<String> a;
+
+	private ArrayList<String> b;
+
+	@FXML
+	private TextField set;
+
+	@FXML
+	void addSet(ActionEvent event) {
+		if (numSet < 3) {
+
 		}
-    	
 
-    }
+	}
 
-    @FXML
-    void operate(ActionEvent event) {
-    	if(setA.getValue() == null) {
-    		Alert alert = new Alert(AlertType.ERROR, "Por favor seleccione un conjunto A", ButtonType.OK);
+	@FXML
+	void operate(ActionEvent event) {
+		if (setA.getValue() == null) {
+			Alert alert = new Alert(AlertType.ERROR, "Por favor seleccione un conjunto A", ButtonType.OK);
 			alert.getDialogPane().setMinHeight(Region.USE_PREF_SIZE);
 			alert.show();
-    	}
-    	
-    	if(setB.getValue() == null) {
-    		Alert alert = new Alert(AlertType.ERROR, "Por favor seleccione un conjunto B", ButtonType.OK);
+		}
+
+		if (setB.getValue() == null) {
+			Alert alert = new Alert(AlertType.ERROR, "Por favor seleccione un conjunto B", ButtonType.OK);
 			alert.getDialogPane().setMinHeight(Region.USE_PREF_SIZE);
 			alert.show();
-    	}
-    	
-    	if(operator.getValue() == null) {
-    		Alert alert = new Alert(AlertType.ERROR, "Por favor seleccione un operador", ButtonType.OK);
+		}
+
+		if (operator.getValue() == null) {
+			Alert alert = new Alert(AlertType.ERROR, "Por favor seleccione un operador", ButtonType.OK);
 			alert.getDialogPane().setMinHeight(Region.USE_PREF_SIZE);
 			alert.show();
-    	}
-    	
-    	
-    	
-    }
+		}
 
-    @FXML
-    void initialize() {
-    	setA.getItems().add("A");
-    	setA.getItems().add("A'");
-    	setA.getItems().add("B");
-    	setA.getItems().add("B'");
-    	
-    	setB.getItems().add("A");
-    	setB.getItems().add("A'");
-    	setB.getItems().add("B");
-    	setB.getItems().add("B'");
-    	
-    	operator.getItems().add("Interseción");
-    	operator.getItems().add("Unión");
-    	operator.getItems().add("Complemento");
-    	operator.getItems().add("Diferencia");
+	}
 
-    	
-    	so = new SetOperations();
-    }
+	/**
+	 * This method is responsible for initializing the elements in the user
+	 * interface, sets and operators.
+	 */
+	@FXML
+	void initialize() {
+		setA.getItems().add("A");
+		setA.getItems().add("A'");
+		setA.getItems().add("B");
+		setA.getItems().add("B'");
+
+		setB.getItems().add("A");
+		setB.getItems().add("A'");
+		setB.getItems().add("B");
+		setB.getItems().add("B'");
+
+		operator.getItems().add("Interseción");
+		operator.getItems().add("Unión");
+		operator.getItems().add("Complemento");
+		operator.getItems().add("Diferencia");
+
+		so = new SetOperations();
+	}
 }
