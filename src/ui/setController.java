330a83eb9ea30434c6063e5a74cae5bd3b55e7fa
Juan David Lectamo Caicedo <1006219058@academico.icesi.edu.co>
2020-02-04 14:28:17 -0500

    First Commit

--- src/ui/setController.java
+++ src/ui/setController.java
@@ -1,5 +1,44 @@
 package ui;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+
 public class setController {
 
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private ComboBox<String> setA;
+
+    @FXML
+    private ComboBox<String> setB;
+
+    @FXML
+    private ComboBox<String> operator;
+
+    @FXML
+    private Label result;
+
+    @FXML
+    void addSet(ActionEvent event) {
+
+    }
+
+    @FXML
+    void operate(ActionEvent event) {
+
+    }
+
+    @FXML
+    void initialize() {
+
+    }
 }
