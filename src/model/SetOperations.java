67478bdaaa11680d41b8a2b439c097fa5483731b
Luis Felipe Sanchez Vega <1007753235@academico.icesi.edu.co>
2020-02-04 15:14:15 -0500

    creating javaDoc

--- src/model/SetOperations.java
+++ src/model/SetOperations.java
@@ -1,17 +1,32 @@
-<<<<<<< HEAD
 package model;
 
 import java.util.HashSet;
 import java.util.Set;
 
-public class SetOperations {
+/**
+* This class defines the basic operations between two sets. 
+* @author Luis Felipe Sanchez Vega
+*/
 
+public class SetOperations {
+	
+	
+	/**
+	 * The constructor of the class <br><br> 
+	 */
 	public SetOperations() {
 
 
 
 	}
-
+	
+	/**
+	 * This method finds the union between two given sets <br><br> 
+	 * <b>Pre: </b> The two sets must not be null <br><br>
+	 * @param setA The first set <br><br>
+	 * @param setB The second set <br><br>
+	 * @return One set with the elements of the two first sets <br><br>
+	 */
 	public Set<String> union(Set<String> setA, Set<String> setB){
 
 		Set<String> setC = new HashSet<String>();
@@ -22,7 +37,14 @@ public class SetOperations {
 		return setC;
 
 	}
-
+	
+	/**
+	 * This method finds the intersection between two given sets <br><br> 
+	 * <b>Pre: </b> The two sets must not be null <br><br>
+	 * @param setA The first set <br><br>
+	 * @param setB The second set <br><br>
+	 * @return One set with the common elements of the two first sets <br><br>
+	 */
 	public Set<String> intersection(Set<String> setA, Set<String> setB){
 
 		Set<String> setC = new HashSet<String>();
@@ -35,7 +57,15 @@ public class SetOperations {
 
 		return setC;
 	}
-
+	
+	
+	/**
+	 * This method finds the diference between two given sets <br><br> 
+	 * <b>Pre: </b> The two sets must not be null <br><br>
+	 * @param setA The first set <br><br>
+	 * @param setB The second set <br><br>
+	 * @return One set whose elements are within setA but not within setB <br><br>
+	 */
 	public Set<String> diference(Set<String> setA, Set<String> setB){
 
 		Set<String> setC = new HashSet<String>();
@@ -50,31 +80,4 @@ public class SetOperations {
 	}
 	
 	
-
-=======
-package model;
-
-import java.util.HashSet;
-import java.util.Set;
-
-public class SetOperations {
-	
-	public SetOperations() {
-		
-		
-		
-	}
-	
-	public Set<String> union(Set<String> setA, Set<String> setB){
-		
-		Set<String> setC = new HashSet<String>();
-		
-		setC.addAll(setA);
-		setC.addAll(setB);
-		
-		return setC;
-		
-	}
-	
->>>>>>> cdd1ba3f8d08f6942509474231dbd24f9c92c3cf
 }
